d9687c561e98bd1bca2796643a525a7c9a0b70d7
Abdulbasit Adio <94764960+Ayam7273@users.noreply.github.com>
2024-06-26 02:14:39 +0100

    3rd commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 import 'package:brickmortar/features/app/splash_screen/splash_screen.dart';
-import 'package:brickmortar/features/user_auth/presentation/pages/home_page.dart';
 import 'package:brickmortar/features/user_auth/presentation/pages/login_page.dart';
 import 'package:brickmortar/features/user_auth/presentation/pages/sign_out_page.dart';
 import 'package:brickmortar/features/user_auth/presentation/pages/sign_up_page.dart';
+import 'package:brickmortar/view/home/home_view.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         '/login': (context) => LoginPage(),
         '/signUp': (context) => SignUpPage(),
-        '/home': (context) => HomePage(),
+        '/home': (context) => HomeView(),
         '/signOut': (context) => SignOutPage(),
 
       },
